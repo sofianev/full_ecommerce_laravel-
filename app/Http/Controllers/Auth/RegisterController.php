4e6fe3fedb465de97cfa9d0941ca2c36eed093a6
sofianebouzid4@gmail.com <sofianebouzid4@gmail.com>
2021-03-30 19:37:00 +0200

    new laravel project

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,7 +48,7 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {   
-       
+      
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255'],
@@ -64,8 +64,8 @@ class RegisterController extends Controller
      * @return \App\User
      */
     protected function create(array $data)
-    {  dd($data);
-
+    {  
+ dd($data);
         return User::create([
             
             'name' => $data['name'],
